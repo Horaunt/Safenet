759c12e44a9218a26e8b51cd1f6302af48b01c6f
MuditaBhathar <mudita.bhathar205@gmail.com>
2024-03-18 03:27:12 +0530

    login

--- components/Login.jsx
+++ components/Login.jsx
@@ -11,12 +11,14 @@ console.log('Logging in with:', username, password);
 }
 
 return (
-<div>
+<div className='login'>
     <h1 className='heading'>Login Page</h1>
     <form onSubmit={handleLogin}>
     <div>
-        <label htmlFor="username">Username:</label>
+        <label htmlFor="username">Username:  </label>
+        <br></br>
         <input 
+        placeholder='Username'
         type="text" 
         id="username" 
         value={username} 
@@ -24,8 +26,10 @@ return (
         />
     </div>
     <div>
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password:  </label>
+        <br></br>
         <input 
+        placeholder='Password'
         type="password" 
         id="password" 
         value={password} 
