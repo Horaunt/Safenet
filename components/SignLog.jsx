5aa6022c9e94e0470ed2e6ab99becf72f89852ea
MuditaBhathar <mudita.bhathar205@gmail.com>
2024-03-17 23:27:14 +0530

    initial commit

---
+++ components/SignLog.jsx
@@ -0,0 +1,9 @@
+const SignLog=()=>{
+    return <main>
+        <div className="btn">
+        <button className="sign">Sign Up</button>
+        <button className="log">Login in</button>
+        </div>
+    </main>   
+}
+export default SignLog;
