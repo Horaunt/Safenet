cb2c186d62f5e77fedb7130975f359c6b9d30ddc
Zaman00007 <shahidzaman75724@gmail.com>
2024-03-19 04:00:39 +0530

    final touch ups

--- src/constants/index.js
+++ src/constants/index.js
@@ -48,22 +48,12 @@ export const navLinks = [
 ];
 
 const services = [
-  {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
+ 
   {
     title: "Cybersecurity Expert",
     icon: backend,
   },
-  {
-    title: "Flutter Developer",
-    icon: creator,
-  },
+  
 ];
 
 const technologies = [
@@ -176,22 +166,7 @@ const testimonials = [
     company: "Acme Co",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
+  
 ];
 
 const projects = [
